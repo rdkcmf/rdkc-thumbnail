e5812f13a4a898ecc09d23c938c3bedf574713d4
gnanesha.gowda <gnanesha.gowda@ltts.com>
2020-11-02 14:40:40 +0000

    SUMMIT2020-3: Detect face or person in front of the camera.
    Reason for change: Able to detect face in front of the camera with opencv component.
    Test Procedure: Validate with face_detection binary.
    Risks: None
    
    Signed-off-by: gnanesha.gowda <gnanesha.gowda@ltts.com>
    Change-Id: I97678fffa3567beea20fbecbc28d2252577ec6fe

---
+++ face_detection/face_detection.cpp
@@ -0,0 +1,84 @@
+
+/**
+##########################################################################
+# If not stated otherwise in this file or this component's LICENSE
+# file the following copyright and licenses apply:
+#
+# Copyright 2019 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+**/
+
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main( int argc, const char** argv );
+void detectAndDraw( Mat& img, CascadeClassifier& cascade );
+
+/* {{{ main() */
+int main( int argc, const char** argv )
+{
+    VideoCapture capture;
+    Mat frame;
+
+    CascadeClassifier cascade;
+
+    cascade.load( "/lib/rdk/haarcascade_frontalface_alt.xml" ) ;
+
+    capture.open(0);
+
+    if( capture.isOpened() )
+    {
+        printf(" \n Face Detection Started....");
+        
+        while(1)
+        {
+            capture >> frame;
+
+            if( frame.empty() )
+                break;
+
+            Mat frame1 = frame.clone();
+
+            detectAndDraw( frame1, cascade );
+        }
+    }
+    else
+    {
+        printf("\n Could not open camera");
+    }
+}
+/* }}} */
+
+/* {{{ detectAndDraw() */
+void detectAndDraw( Mat& img, CascadeClassifier& cascade )
+{
+    vector<Rect> faces;
+
+    cascade.detectMultiScale( img, faces, 1.1,
+                             2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+
+    if( faces.size() >= 1 )
+    {
+        printf("\n Face Detected From Live Camera Buffer...");
+    }
+    
+}
+/* }}} */
+
